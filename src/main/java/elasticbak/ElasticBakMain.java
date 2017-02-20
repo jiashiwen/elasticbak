76b4138204f3fe5ab97826785de5d5b438f40dfe
jiashiwen <shiwen4@leju.com>
2017-02-20 18:15:17 +0800

    change main class

--- src/main/java/elasticbak/ElasticBakMain.java
+++ src/main/java/elasticbak/ElasticBakMain.java
@@ -27,11 +27,11 @@ public class ElasticBakMain {
 		ArgsSettingEntity argssetting = new ArgsSettingEntity();
 		ElasticsearchCopyIndex cpidx = new ElasticsearchCopyIndex();
 		ElasticsearchIndexTools esidxtools = new ElasticsearchIndexTools();
-		Client sourceclient;
-		Client targetclient;
+		Client client;
 
 		JCommander jc = new JCommander();
-		jc.setProgramName("java -jar elasticbak.jar");
+		jc.setProgramName("java -jar elasticbak.jar <exp/imp>");
+	
 		jc.addObject(argssetting);
 
 		/*
@@ -41,86 +41,95 @@ public class ElasticBakMain {
 			jc.parse(args);
 		} catch (Exception e) {
 			jc.usage();
+			e.printStackTrace();
 			System.exit(0);
 		}
-
+		
 		if (argssetting.isHelp()) {
 			jc.usage();
 			System.exit(0);
 		}
 
-		// 解析脚本文件并执行相关操作
-		if (argssetting.getScript_file() != null) {
-			String scriptstring = new JsonUtilities().ReadJsonFile(argssetting.getScript_file());
-			ScriptEntity script = (ScriptEntity) objectMapper.readValue(scriptstring, ScriptEntity.class);
-			String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(script);
-			logger.info("Your Script setting is: \n\t" + json);
-			JsonNode node = objectMapper.readTree(scriptstring);
-
-			if (node.get("indexes") == null) {
-				logger.info("Script 'indexes' must be set!");
-				return;
-			}
-
-			List<IndexesRelationEntity> indexrelation = script.getIndexesrelation();
-
-			for (IndexesRelationEntity idx : indexrelation) {
-				sourceclient = new ElasticsearchConnector(script.getSource_cluster(), script.getSource_host(),
-						script.getSource_port()).getClient();
-				targetclient = new ElasticsearchConnector(script.getTarget_cluster(), script.getTarget_host(),
-						script.getTarget_port()).getClient();
-				String idxjson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(idx);
-				logger.info("\n\t" + idxjson);
-				if (idx.getSource_index() == null || idx.getTarget_index() == null) {
-					logger.info("In script 'indexes.source_index' and 'indexes.target_index' must been set!");
-					continue;
-				}
-
-				switch (idx.getType().toUpperCase()) {
-				case "DATA":
-					System.out.println("TYPE IS DATA OK!");
-					if (idx.getDsl() != null) {
-						cpidx.CopyIndexByQueryDsl(sourceclient, idx.getSource_index(), targetclient,
-								idx.getTarget_index(), idx.getDsl().toString());
-
-					} else {
-						cpidx.CopyIndex(sourceclient, idx.getSource_index(), targetclient, idx.getTarget_index());
-					}
-					break;
-				case "META":
-					System.out.println("TYPE IS META OK!");
-					cpidx.CopyIndexMetadata(sourceclient, idx.getSource_index(), targetclient, idx.getTarget_index());
-					break;
-				case "FORCE":
-					System.out.println("FORCE OK!");
-					if (esidxtools.IndexExistes(targetclient, idx.getTarget_index())) {
-						esidxtools.DeleteIndex(targetclient, idx.getTarget_index());
-					}
-
-					cpidx.CopyIndexMetadata(sourceclient, idx.getSource_index(), targetclient, idx.getTarget_index());
-
-					if (idx.getDsl() != null) {
-						cpidx.CopyIndexByQueryDsl(sourceclient, idx.getSource_index(), targetclient,
-								idx.getTarget_index(), idx.getDsl().toString());
-
-					} else {
-						cpidx.CopyIndex(sourceclient, idx.getSource_index(), targetclient, idx.getTarget_index());
-					}
-					break;
-				default:
-					System.out.println("type must be set [data,meta,force]");
-					break;
-				}
-
-				sourceclient.close();
-				targetclient.close();
-
-			}
-
+		//计算exp和imp做异或运算即次两个参数必须有一个且只有一个为真
+		if(!(argssetting.isExp()^argssetting.isImp())){
+			jc.usage();
 			System.exit(0);
 		}
+		
 
-		if (argssetting.getSource_index() == null || argssetting.getTarget_index() == null) {
+		
+		// 解析脚本文件并执行相关操作
+//		if (argssetting.getScript_file() != null) {
+//			String scriptstring = new JsonUtilities().ReadJsonFile(argssetting.getScript_file());
+//			ScriptEntity script = (ScriptEntity) objectMapper.readValue(scriptstring, ScriptEntity.class);
+//			String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(script);
+//			logger.info("Your Script setting is: \n\t" + json);
+//			JsonNode node = objectMapper.readTree(scriptstring);
+//
+//			if (node.get("indexes") == null) {
+//				logger.info("Script 'indexes' must be set!");
+//				return;
+//			}
+//
+//			List<IndexesRelationEntity> indexrelation = script.getIndexesrelation();
+//
+//			for (IndexesRelationEntity idx : indexrelation) {
+//				sourceclient = new ElasticsearchConnector(script.getSource_cluster(), script.getSource_host(),
+//						script.getSource_port()).getClient();
+//				targetclient = new ElasticsearchConnector(script.getTarget_cluster(), script.getTarget_host(),
+//						script.getTarget_port()).getClient();
+//				String idxjson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(idx);
+//				logger.info("\n\t" + idxjson);
+//				if (idx.getSource_index() == null || idx.getTarget_index() == null) {
+//					logger.info("In script 'indexes.source_index' and 'indexes.target_index' must been set!");
+//					continue;
+//				}
+//
+//				switch (idx.getType().toUpperCase()) {
+//				case "DATA":
+//					System.out.println("TYPE IS DATA OK!");
+//					if (idx.getDsl() != null) {
+//						cpidx.CopyIndexByQueryDsl(sourceclient, idx.getSource_index(), targetclient,
+//								idx.getTarget_index(), idx.getDsl().toString());
+//
+//					} else {
+//						cpidx.CopyIndex(sourceclient, idx.getSource_index(), targetclient, idx.getTarget_index());
+//					}
+//					break;
+//				case "META":
+//					System.out.println("TYPE IS META OK!");
+//					cpidx.CopyIndexMetadata(sourceclient, idx.getSource_index(), targetclient, idx.getTarget_index());
+//					break;
+//				case "FORCE":
+//					System.out.println("FORCE OK!");
+//					if (esidxtools.IndexExistes(targetclient, idx.getTarget_index())) {
+//						esidxtools.DeleteIndex(targetclient, idx.getTarget_index());
+//					}
+//
+//					cpidx.CopyIndexMetadata(sourceclient, idx.getSource_index(), targetclient, idx.getTarget_index());
+//
+//					if (idx.getDsl() != null) {
+//						cpidx.CopyIndexByQueryDsl(sourceclient, idx.getSource_index(), targetclient,
+//								idx.getTarget_index(), idx.getDsl().toString());
+//
+//					} else {
+//						cpidx.CopyIndex(sourceclient, idx.getSource_index(), targetclient, idx.getTarget_index());
+//					}
+//					break;
+//				default:
+//					System.out.println("type must be set [data,meta,force]");
+//					break;
+//				}
+//
+//				sourceclient.close();
+//				targetclient.close();
+//
+//			}
+//
+//			System.exit(0);
+//		}
+
+		if (argssetting.getIndex() == null ) {
 			logger.info("index name must been set!");
 			jc.usage();
 			System.exit(0);
@@ -129,55 +138,55 @@ public class ElasticBakMain {
 		String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(argssetting);
 		logger.info("Your command line setting is: \n\t" + json);
 
-		sourceclient = new ElasticsearchConnector(argssetting.getSource_cluster(), argssetting.getSource_host(),
-				argssetting.getSource_port()).getClient();
-		targetclient = new ElasticsearchConnector(argssetting.getTarget_cluster(), argssetting.getTarget_host(),
-				argssetting.getTarget_port()).getClient();
+		client = new ElasticsearchConnector(argssetting.getCluster(), argssetting.getHost(),
+				argssetting.getPort()).getClient();
 
 		System.out.println(argssetting.getDsl());
 
-		switch (argssetting.getType().toUpperCase()) {
-		case "DATA":
-			System.out.println("TYPE IS DATA OK!");
-			if (argssetting.getDsl() != null) {
-				cpidx.CopyIndexByQueryDsl(sourceclient, argssetting.getSource_index(), targetclient,
-						argssetting.getTarget_index(), argssetting.getDsl());
+		Client targetclient;
+	  
+//		switch (argssetting.getType().toUpperCase()) {
+//		case "DATA":
+//			System.out.println("TYPE IS DATA OK!");
+//			if (argssetting.getDsl() != null) {
+//				cpidx.CopyIndexByQueryDsl(client, argssetting.getIndex(), targetclient,
+//						argssetting.getTarget_index(), argssetting.getDsl());
+//
+//			} else {
+//				cpidx.CopyIndex(client, argssetting.getIndex(), targetclient,
+//						argssetting.getTarget_index());
+//			}
+//			break;
+//		case "META":
+//			System.out.println("TYPE IS META OK!");
+//			cpidx.CopyIndexMetadata(client, argssetting.getIndex(), targetclient,
+//					argssetting.getTarget_index());
+//			break;
+//		case "FORCE":
+//			System.out.println("FORCE OK!");
+//			if (esidxtools.IndexExistes(targetclient, argssetting.getTarget_index())) {
+//				esidxtools.DeleteIndex(targetclient, argssetting.getTarget_index());
+//			}
+//
+//			cpidx.CopyIndexMetadata(client, argssetting.getIndex(), targetclient,
+//					argssetting.getTarget_index());
+//
+//			if (argssetting.getDsl() != null) {
+//				cpidx.CopyIndexByQueryDsl(client, argssetting.getIndex(), targetclient,
+//						argssetting.getTarget_index(), argssetting.getDsl());
+//
+//			} else {
+//				cpidx.CopyIndex(client, argssetting.getIndex(), targetclient,
+//						argssetting.getTarget_index());
+//			}
+//			break;
+//		default:
+//			System.out.println("type must be set [data,meta,force]");
+//			break;
+//		}
 
-			} else {
-				cpidx.CopyIndex(sourceclient, argssetting.getSource_index(), targetclient,
-						argssetting.getTarget_index());
-			}
-			break;
-		case "META":
-			System.out.println("TYPE IS META OK!");
-			cpidx.CopyIndexMetadata(sourceclient, argssetting.getSource_index(), targetclient,
-					argssetting.getTarget_index());
-			break;
-		case "FORCE":
-			System.out.println("FORCE OK!");
-			if (esidxtools.IndexExistes(targetclient, argssetting.getTarget_index())) {
-				esidxtools.DeleteIndex(targetclient, argssetting.getTarget_index());
-			}
+		client.close();
 
-			cpidx.CopyIndexMetadata(sourceclient, argssetting.getSource_index(), targetclient,
-					argssetting.getTarget_index());
-
-			if (argssetting.getDsl() != null) {
-				cpidx.CopyIndexByQueryDsl(sourceclient, argssetting.getSource_index(), targetclient,
-						argssetting.getTarget_index(), argssetting.getDsl());
-
-			} else {
-				cpidx.CopyIndex(sourceclient, argssetting.getSource_index(), targetclient,
-						argssetting.getTarget_index());
-			}
-			break;
-		default:
-			System.out.println("type must be set [data,meta,force]");
-			break;
-		}
-
-		sourceclient.close();
-		targetclient.close();
 
 	}
 
--- src/main/java/elasticbak/Entities/ArgsSettingEntity.java
+++ src/main/java/elasticbak/Entities/ArgsSettingEntity.java
@@ -7,29 +7,26 @@ public class ArgsSettingEntity {
 	@Parameter(names = "--help", help = true)
 	private boolean help = false;
 
-	@Parameter(names = "--source_cluster", description = "Source elasticsearch cluster name,default is 'elasticsearch")
-	private String source_cluster = "elasticsearch";
+	@Parameter(names = "--exp", description = "export mode",arity=0)
+	private boolean exp = false;
 
-	@Parameter(names = "--source_host", description = "Source elasticsearch cluster one of master ip address,default is '127.0.0.1'.")
-	private String source_host = "127.0.0.1";
+	@Parameter(names = "--imp", description = "import mode",arity=0)
+	private boolean imp = false;
 
-	@Parameter(names = "--source_port", description = "Source port")
-	private int source_port = 9300;
+	@Parameter(names = "--cluster", description = "Elasticsearch cluster name,default is 'elasticsearch")
+	private String cluster = "elasticsearch";
 
-	@Parameter(names = "--source_index", required = false, description = "Source index name")
-	private String source_index;
+	@Parameter(names = "--host", description = "Elasticsearch cluster one of master ip address,default is '127.0.0.1'.")
+	private String host = "127.0.0.1";
 
-	@Parameter(names = "--target_cluster", description = "Target elasticsearch cluster name,default is 'elasticsearch")
-	private String target_cluster = "elasticsearch";
+	@Parameter(names = "--port", description = "Elasticsearch port,default is 9300")
+	private int port = 9300;
 
-	@Parameter(names = "--target_host", description = "Target elasticsearch cluster one of master ip address,default is '127.0.0.1'.")
-	private String target_host = "127.0.0.1";
-
-	@Parameter(names = "--target_port", description = "Target port")
-	private int target_port = 9300;
-
-	@Parameter(names = "--target_index", required = false, description = "Target index name")
-	private String target_index;
+	@Parameter(names = "--index", required = false, description = "Index name")
+	private String index;
+	
+	@Parameter(names="--dir",description ="Backup directory,default is current")
+	private String dir="./";
 
 	@Parameter(names = "--type", description = "Transfor type value is [data,meta,force] and default value is 'meta'.If value is 'metadata' try to create a new empty target index as source;'data' copy source index documents to target index; 'force' delete target index if exists and copy source index to target index.")
 	private String type = "meta";
@@ -40,12 +37,68 @@ public class ArgsSettingEntity {
 	@Parameter(names = "--script_file", description = "execute script file write by json ")
 	private String script_file;
 
-	public String getScript_file() {
-		return script_file;
+	public boolean isHelp() {
+		return help;
 	}
 
-	public void setScript_file(String script_file) {
-		this.script_file = script_file;
+	public void setHelp(boolean help) {
+		this.help = help;
+	}
+
+	public boolean isExp() {
+		return exp;
+	}
+
+	public void setExp(boolean exp) {
+		this.exp = exp;
+	}
+
+	public boolean isImp() {
+		return imp;
+	}
+
+	public void setImp(boolean imp) {
+		this.imp = imp;
+	}
+
+	public String getCluster() {
+		return cluster;
+	}
+
+	public void setCluster(String cluster) {
+		this.cluster = cluster;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 	public String getDsl() {
@@ -56,84 +109,12 @@ public class ArgsSettingEntity {
 		this.dsl = dsl;
 	}
 
-	public boolean isHelp() {
-		return help;
+	public String getScript_file() {
+		return script_file;
 	}
 
-	public void setHelp(boolean help) {
-		this.help = help;
-	}
-
-	public String getSource_cluster() {
-		return source_cluster;
-	}
-
-	public void setSource_cluster(String source_cluster) {
-		this.source_cluster = source_cluster;
-	}
-
-	public String getSource_host() {
-		return source_host;
-	}
-
-	public void setSource_host(String source_host) {
-		this.source_host = source_host;
-	}
-
-	public int getSource_port() {
-		return source_port;
-	}
-
-	public void setSource_port(int source_port) {
-		this.source_port = source_port;
-	}
-
-	public String getSource_index() {
-		return source_index;
-	}
-
-	public void setSource_index(String source_index) {
-		this.source_index = source_index;
-	}
-
-	public String getTarget_cluster() {
-		return target_cluster;
-	}
-
-	public void setTarget_cluster(String target_cluster) {
-		this.target_cluster = target_cluster;
-	}
-
-	public String getTarget_host() {
-		return target_host;
-	}
-
-	public void setTarget_host(String target_host) {
-		this.target_host = target_host;
-	}
-
-	public int getTarget_port() {
-		return target_port;
-	}
-
-	public void setTarget_port(int target_port) {
-		this.target_port = target_port;
-	}
-
-	public String getTarget_index() {
-		return target_index;
-	}
-
-	public void setTarget_index(String target_index) {
-		this.target_index = target_index;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
+	public void setScript_file(String script_file) {
+		this.script_file = script_file;
 	}
 
 }
